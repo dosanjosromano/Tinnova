8e27eead677b8958433c159202fe87536d7d5b58
Romano Bernardino <romano.bernardino@lumis.com.br>
2023-05-24 23:58:44 -0300

    Implementa Camada de API GetTodosVeiculos

--- API-Veiculo/src/main/java/br/com/tinnova/veiculo/application/api/VeiculoAPI.java
+++ API-Veiculo/src/main/java/br/com/tinnova/veiculo/application/api/VeiculoAPI.java
@@ -1,6 +1,9 @@
 package br.com.tinnova.veiculo.application.api;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,4 +19,8 @@ public interface VeiculoAPI {
 	@PostMapping
 	@ResponseStatus(code = HttpStatus.CREATED)
 	VeiculoResponse postVeiculo(@Valid @RequestBody VeiculoRequest veiculoRequest);
+	
+	@GetMapping
+	@ResponseStatus(code = HttpStatus.OK)
+    List<VeiculoListResponse> getTodosVeiculos();
 }
