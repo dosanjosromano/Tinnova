016e8878ae987ffc54b5931a43f56323ea4cbc8c
Romano Bernardino <romano.bernardino@lumis.com.br>
2023-05-25 00:00:46 -0300

    Implementa Camada de Service GetTodosVeiculos com fluxo para Repository

--- API-Veiculo/src/main/java/br/com/tinnova/veiculo/application/repository/VeiculoRepository.java
+++ API-Veiculo/src/main/java/br/com/tinnova/veiculo/application/repository/VeiculoRepository.java
@@ -1,9 +1,13 @@
 package br.com.tinnova.veiculo.application.repository;
 
+import java.util.List;
+
 import br.com.tinnova.domain.Veiculo;
 
 public interface VeiculoRepository {
 
 	Veiculo salva(Veiculo veiculo);
+
+	List<Veiculo> buscaTodosVeiculos();
 
 }
--- API-Veiculo/src/main/java/br/com/tinnova/veiculo/application/service/VeiculoApplicationService.java
+++ API-Veiculo/src/main/java/br/com/tinnova/veiculo/application/service/VeiculoApplicationService.java
@@ -1,8 +1,11 @@
 package br.com.tinnova.veiculo.application.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import br.com.tinnova.domain.Veiculo;
+import br.com.tinnova.veiculo.application.api.VeiculoListResponse;
 import br.com.tinnova.veiculo.application.api.VeiculoRequest;
 import br.com.tinnova.veiculo.application.api.VeiculoResponse;
 import br.com.tinnova.veiculo.application.repository.VeiculoRepository;
@@ -24,6 +27,14 @@ public class VeiculoApplicationService implements VeiculoService {
 		return VeiculoResponse.builder()
 				.idVeiculo(veiculo.getIdVeiculo())
 				.build();
+	}
+
+	@Override
+	public List<VeiculoListResponse> buscaTodosVeiculos() {
+		log.info("[inicia] VeiculoApplicationService - buscaTodosVeiculos ");
+		List<Veiculo> veiculos = veiculoRepository.buscaTodosVeiculos();
+		log.info("[finaliza] VeiculoApplicationService - buscaTodosVeiculos ");
+		return VeiculoListResponse.converte(veiculos);
 	}
 
 }
